563bd3dfd0569e857916342257f19764aa40e6b2
athesto <gamez.live@gmail.com>
2026-03-15 12:37:08 -0500

    add instert

--- OmniStock/Forms/UserForm.vb
+++ OmniStock/Forms/UserForm.vb
@@ -1,5 +1,6 @@
 ﻿Public Class UserForm
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim dao As New UserDAO()
+        dao.Insert(txtEmail.Text, txtPassword.Text, cmbRole.SelectedItem.ToString())
     End Sub
 End Class
