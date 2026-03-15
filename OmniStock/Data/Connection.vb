2cb398c28d21f2eea49ebc5a8bf00ac527f59a79
athesto <gamez.live@gmail.com>
2026-03-15 12:29:43 -0500

    set testpass as blank

--- OmniStock/Data/Connection.vb
+++ OmniStock/Data/Connection.vb
@@ -2,7 +2,7 @@
 
 Public Class Connection
     Private Shared cadena As String =
-    "server=localhost;port=3306;database=omnistock;user=root;password=pa$$;"
+    "server=localhost;port=3306;database=omnistock;user=root;password=;"
 
     Public Shared Function GetConnection() As MySqlConnection
         Dim conexion As New MySqlConnection(cadena)
