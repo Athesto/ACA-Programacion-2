c1b94248876d5c9fa0a5da439acb15ed63c2763c
athesto <gamez.live@gmail.com>
2026-03-15 15:26:38 -0500

    Inventory and Product DAO

---
+++ OmniStock/Data/InventoryDAO.vb
@@ -0,0 +1,21 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.Data
+
+Public Class InventoryDAO
+
+    Public Function GetAll() As DataTable
+        Dim sql As String = "SELECT id, email, role FROM users"
+        Return DbHelper.ExecuteDataTable(sql)
+    End Function
+
+    Public Function Insert(productId As String, units As Integer) As Integer
+        Dim sql As String =
+            "INSERT INTO inventory (product_id, units) VALUES (@product_id, @units)"
+
+        Dim parameters As New List(Of MySqlParameter) From {
+            New MySqlParameter("@product_id", productId),
+            New MySqlParameter("@units", units)
+        }
+        Return DbHelper.ExecuteNonQuery(sql, parameters)
+    End Function
+End Class
---
+++ OmniStock/Data/ProductDAO.vb
@@ -0,0 +1,25 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.Data
+
+Public Class ProductDAO
+
+    Public Function GetAll() As DataTable
+        Dim sql As String = "SELECT name, sku, description, price FROM products"
+        Return DbHelper.ExecuteDataTable(sql)
+    End Function
+
+    Public Function Insert(name As String, sku As String, description As String, price As Decimal) As Integer
+        Dim sql As String =
+            "INSERT INTO products (name, price, sku, description) VALUES (@name, @price, @sku, @description)"
+
+        Dim parameters As New List(Of MySqlParameter) From {
+            New MySqlParameter("@name", name),
+            New MySqlParameter("@price", price),
+            New MySqlParameter("@sku", sku),
+            New MySqlParameter("@description", description)
+        }
+        DbHelper.ExecuteNonQuery(sql, parameters)
+        Return DbHelper.ExecuteScalar("SELECT LAST_INSERT_ID();")
+    End Function
+
+End Class
