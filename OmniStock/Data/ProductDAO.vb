82ce1bb26f46ed7af22f6d340e368eda8b2ab9b6
athesto <gamez.live@gmail.com>
2026-03-15 17:54:16 -0500

    incluir todos los sku y los valores por sku

--- OmniStock/Data/ProductDAO.vb
+++ OmniStock/Data/ProductDAO.vb
@@ -8,6 +8,11 @@ Public Class ProductDAO
         Return DbHelper.ExecuteDataTable(sql)
     End Function
 
+    Public Function GetAllSku() As DataTable
+        Dim sql As String = "SELECT sku FROM products"
+        Return DbHelper.ExecuteDataTable(sql)
+    End Function
+
     Public Function Insert(name As String, sku As String, description As String, price As Decimal) As Integer
         Dim sql As String =
             "INSERT INTO products (name, price, sku, description) VALUES (@name, @price, @sku, @description)"
@@ -20,6 +25,15 @@ Public Class ProductDAO
         }
         DbHelper.ExecuteNonQuery(sql, parameters)
         Return DbHelper.ExecuteScalar("SELECT LAST_INSERT_ID();")
+    End Function
+    Public Function GetOneBySku(sku As String) As DataTable
+        Dim sql As String =
+            "SELECT p.sku, p.name, p.price, i.units, p.description FROM products p JOIN inventory i ON p.id = i.product_id where p.sku = (@sku);"
+
+        Dim parameters As New List(Of MySqlParameter) From {
+            New MySqlParameter("@sku", sku)
+        }
+        Return DbHelper.ExecuteDataTable(sql, parameters)
     End Function
 
 End Class
