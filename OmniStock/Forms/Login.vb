52dddd763aaf9dc4f8dd4833fbf27cbe4fb08924
athesto <gamez.live@gmail.com>
2026-03-15 12:36:36 -0500

    use guard clause

--- OmniStock/Forms/Login.vb
+++ OmniStock/Forms/Login.vb
@@ -1,50 +1,17 @@
 ﻿Imports MySql.Data.MySqlClient
-Imports Mysqlx
-
 Public Class Login
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
-    End Sub
-
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
-
-    End Sub
-
-    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
-
-    End Sub
-
-    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Try
-            Using conn As MySqlConnection = Connection.GetConnection()
-                Dim sql As String =
-                    "SELECT id, email, rol FROM users WHERE email = @email AND pass = @pass"
+        Dim dao As New UserDAO()
+        txtEmail.Text = "admin@omnistock.com"
+        txtPassword.Text = "admin123"
 
-                Dim dataEmail As String = "admin@omnistock.com"
-                Dim dataPass As String = "admin123"
+        If Not dao.ValidateLogin(txtEmail.Text, txtPassword.Text) Then
+            MessageBox.Show("Credenciales inválidas")
+            Exit Sub
+        End If
 
-                Using cmd As New MySqlCommand(sql, conn)
-                    cmd.Parameters.AddWithValue("@email", dataEmail)
-                    cmd.Parameters.AddWithValue("@pass", dataPass)
-
-                    Using reader As MySqlDataReader = cmd.ExecuteReader()
-                        If reader.Read() Then
-                            MessageBox.Show("Login correcto")
-                        Else
-                            MessageBox.Show("Credenciales inválidas")
-                        End If
-                    End Using
-                End Using
-            End Using
-
-        Catch ex As Exception
-            MessageBox.Show("Error: " & ex.Message)
-        End Try
-
+        MessageBox.Show("Bienvenido")
         Me.Hide()
         Dim controlPanel As New ControlPanel()
         controlPanel.ShowDialog()
