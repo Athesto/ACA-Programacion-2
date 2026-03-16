0b03a067791d39fd3bbd9f710edd64112a653d03
athesto <gamez.live@gmail.com>
2026-03-15 20:04:20 -0500

    add product update and get id

--- OmniStock/Data/ProductDAO.vb
+++ OmniStock/Data/ProductDAO.vb
@@ -1,4 +1,5 @@
-﻿Imports MySql.Data.MySqlClient
+﻿Imports Google.Protobuf.WellKnownTypes
+Imports MySql.Data.MySqlClient
 Imports System.Data
 
 Public Class ProductDAO
@@ -11,6 +12,14 @@ Public Class ProductDAO
     Public Function GetAllSku() As DataTable
         Dim sql As String = "SELECT sku FROM products"
         Return DbHelper.ExecuteDataTable(sql)
+    End Function
+
+    Public Function GetIdBySku(sku As String) As Integer
+        Dim sql As String = "SELECT p.id FROM products as p where sku=@sku"
+        Dim parameters As New List(Of MySqlParameter) From {
+            New MySqlParameter("@sku", sku)
+        }
+        Return DbHelper.ExecuteScalar(sql, parameters)
     End Function
 
     Public Function Insert(name As String, sku As String, description As String, price As Decimal) As Integer
@@ -36,4 +45,27 @@ Public Class ProductDAO
         Return DbHelper.ExecuteDataTable(sql, parameters)
     End Function
 
+    Public Function UpdateOneBySku(name As String, price As Decimal, sku As String, description As String) As Integer
+        Dim sql As String =
+            "UPDATE products SET name=@name, price=@price, description=@description WHERE sku = (@sku);"
+
+        Dim parameters As New List(Of MySqlParameter) From {
+            New MySqlParameter("@name", name),
+            New MySqlParameter("@price", price),
+            New MySqlParameter("@description", description),
+            New MySqlParameter("@sku", sku)
+        }
+        DbHelper.ExecuteNonQuery(sql, parameters)
+
+        Dim sql2 As String = String.Format("SELECT id FROM products WHERE sku = '{0}'", sku)
+        Return DbHelper.ExecuteScalar(sql2)
+    End Function
+
+    Public Function DelectBySku(sku As String) As DataTable
+        Dim sql As String = "DELETE FROM products where sku=@sku"
+        Dim parameters As New List(Of MySqlParameter) From {
+            New MySqlParameter("@sku", sku)
+        }
+        Return DbHelper.ExecuteScalar(sql, parameters)
+    End Function
 End Class
