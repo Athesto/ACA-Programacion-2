94b275a0d82390af91abcf1effdd3044a2cf4661
athesto <gamez.live@gmail.com>
2026-03-15 20:03:57 -0500

    add update inventory

--- OmniStock/Data/InventoryDAO.vb
+++ OmniStock/Data/InventoryDAO.vb
@@ -18,4 +18,25 @@ Public Class InventoryDAO
         }
         Return DbHelper.ExecuteNonQuery(sql, parameters)
     End Function
+
+    Public Function UpdateOneBySku(product_id As String, units As Decimal) As DataTable
+        Dim sql As String =
+            "UPDATE inventory SET units=@units WHERE product_id = (@product_id);"
+
+        Dim parameters As New List(Of MySqlParameter) From {
+            New MySqlParameter("@product_id", product_id),
+            New MySqlParameter("@units", units)
+        }
+        Return DbHelper.ExecuteDataTable(sql, parameters)
+    End Function
+
+    Public Function DeleteByProductId(product_id As Integer) As Integer
+        Dim sql As String =
+            "DELETE FROM inventory WHERE product_id = (@product_id);"
+
+        Dim parameters As New List(Of MySqlParameter) From {
+            New MySqlParameter("@product_id", product_id)
+        }
+        Return DbHelper.ExecuteNonQuery(sql, parameters)
+    End Function
 End Class
