fef0f1744b931d0d1c1daeb7063302274d0547df
athesto <gamez.live@gmail.com>
2026-03-15 20:04:59 -0500

    update control panel

--- OmniStock/Forms/ControlPanel.vb
+++ OmniStock/Forms/ControlPanel.vb
@@ -16,8 +16,8 @@
     End Sub
 
     Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
-        Dim product_dao As New ProductDAO()
-        Dim skus = product_dao.GetAllSku()
+        Dim product_dao As New ProductDAO
+        Dim skus = product_dao.GetAllSku
         cmbSku.DataSource = skus
         cmbSku.DisplayMember = "sku"
         cmbSku.ValueMember = "sku"
@@ -25,7 +25,6 @@
 
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
         Dim product_dao As New ProductDAO()
-        MessageBox.Show(cmbSku.Text)
         Dim product = product_dao.GetOneBySku(cmbSku.Text)
 
         txtName.Text = product.Rows(0).Item("name").ToString()
@@ -35,4 +34,46 @@
         txtStock.Text = product.Rows(0).Item("units").ToString()
     End Sub
 
+    Private Sub Edit_Click(sender As Object, e As EventArgs) Handles Edit.Click
+        txtName.ReadOnly = False
+        rtxtDescription.ReadOnly = False
+        txtPrice.ReadOnly = False
+        txtStock.ReadOnly = False
+
+        txtName.Enabled = True
+        rtxtDescription.Enabled = True
+        txtPrice.Enabled = True
+        txtStock.Enabled = True
+    End Sub
+
+    Private Sub Submit_Click(sender As Object, e As EventArgs) Handles Submit.Click
+        Dim product_dao As New ProductDAO()
+        Dim product = product_dao.UpdateOneBySku(txtName.Text, txtPrice.Text, txtSku.Text, rtxtDescription.Text)
+
+        Dim inventory_dao As New InventoryDAO()
+        inventory_dao.UpdateOneBySku(txtSku.Text, txtStock.Text)
+
+        txtName.ReadOnly = True
+        rtxtDescription.ReadOnly = True
+        txtPrice.ReadOnly = True
+        txtStock.ReadOnly = True
+
+        txtName.Enabled = False
+        rtxtDescription.Enabled = False
+        txtPrice.Enabled = False
+        txtStock.Enabled = False
+    End Sub
+
+    Private Sub Delete_Click(sender As Object, e As EventArgs) Handles Delete.Click
+        Dim product_dao As New ProductDAO()
+        Dim inventory_dao As New InventoryDAO()
+        Dim product_id = product_dao.GetIdBySku(txtSku.Text)
+        inventory_dao.DeleteByProductId(product_id)
+        product_dao.DelectBySku(txtSku.Text)
+        Button5_Click(Nothing, Nothing)
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
 End Class
